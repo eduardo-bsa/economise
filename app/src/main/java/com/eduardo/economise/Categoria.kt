d13c6a6fd2d202213282822c73ad7f210fe8db24
bastos-eduardo <ti.02@brazzo.com.br>
2020-06-11 12:20:55 -0300

    .

--- app/src/main/java/com/eduardo/economise/Categoria.kt
+++ app/src/main/java/com/eduardo/economise/Categoria.kt
@@ -1,7 +1,7 @@
 package com.eduardo.economise
 
 class Categoria (val id: String, val categoria: String, val usuario: String, val tipo: String) {
-
+//
     constructor() : this ("","", "", "") {
 
     }
