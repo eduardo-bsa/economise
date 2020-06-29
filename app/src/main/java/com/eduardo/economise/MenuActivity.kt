87433605179eac07f516d38c4d0c6711fd6e1bc5
bastos-eduardo <ti.02@brazzo.com.br>
2020-06-29 10:36:28 -0300

    Login com o Facebook finalizado.

--- app/src/main/java/com/eduardo/economise/MenuActivity.kt
+++ app/src/main/java/com/eduardo/economise/MenuActivity.kt
@@ -8,14 +8,21 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_menu.*
 
 
 class MenuActivity : AppCompatActivity() {
@@ -25,9 +32,10 @@ class MenuActivity : AppCompatActivity() {
     lateinit var imLogin: ImageView
     lateinit var imEmail:ImageView
     lateinit var google_login_button: Button
-    lateinit var imFace: ImageView
-    lateinit var face_login_button: Button
+    //lateinit var imFace: ImageView
     private lateinit var auth: FirebaseAuth
+
+    var callbackManager: CallbackManager?=null
 
     var googleSignInClient : GoogleSignInClient? = null
     val RC_SIGN_IN = 1000
@@ -44,14 +52,18 @@ class MenuActivity : AppCompatActivity() {
         tvLogin = findViewById(R.id.tvLogin)
         imLogin = findViewById(R.id.imLogin)
         imEmail = findViewById(R.id.imEmail)
-        imFace = findViewById(R.id.imFace)
-        face_login_button = findViewById(R.id.face_login_button)
+        //imFace = findViewById(R.id.imFace)
         google_login_button = findViewById(R.id.google_login_button)
 
         auth = FirebaseAuth.getInstance()
 
+        callbackManager = CallbackManager.Factory.create()
+        btn_login.setReadPermissions("email")
+
+        btn_login.setOnClickListener { signInFace() }
+
         imEmail.bringToFront()
-        imFace.bringToFront()
+        //imFace.bringToFront()
 
         btEmail.setOnClickListener {
             val intent = Intent(this@MenuActivity, CreateAccountActivity::class.java)
@@ -77,14 +89,14 @@ class MenuActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        var gsq = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        val gsq = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
         googleSignInClient = GoogleSignIn.getClient(this, gsq)
 
         google_login_button.setOnClickListener {
-            var signInIntent = googleSignInClient?.signInIntent
+            val signInIntent = googleSignInClient?.signInIntent
             startActivityForResult(signInIntent, RC_SIGN_IN)
         }
     }
@@ -95,14 +107,13 @@ class MenuActivity : AppCompatActivity() {
         if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
-                // Google Sign In was successful, authenticate with Firebase
                 val account = task.getResult(ApiException::class.java)
                 firebaseAuthWithGoogle(account!!)
             } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
                 Log.w("TAG", "Google sign in failed", e)
-                // ...
             }
+        } else {
+            callbackManager!!.onActivityResult(requestCode, resultCode, data)
         }
     }
 
@@ -121,6 +132,37 @@ class MenuActivity : AppCompatActivity() {
                 } else {
                     Toast.makeText(this, "erro", Toast.LENGTH_LONG).show()
                 }
+            }
+    }
+
+    private fun signInFace() {
+        btn_login.registerCallback(callbackManager, object: FacebookCallback<LoginResult>{
+            override fun onSuccess(result: LoginResult?) {
+                handleFacebookAccessToken(result!!.accessToken)
+            }
+
+            override fun onCancel() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onError(error: FacebookException?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    private fun handleFacebookAccessToken(accessToken: AccessToken?) {
+        val credential = FacebookAuthProvider.getCredential(accessToken!!.token)
+        auth.signInWithCredential(credential)
+            .addOnFailureListener {e ->
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            }
+            .addOnSuccessListener {result ->
+                val intent = Intent(this@MenuActivity, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                startActivity(intent)
             }
     }
 }
